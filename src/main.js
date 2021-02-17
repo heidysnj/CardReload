d72043f71c8ad1aa70313522783d52ea548c298a
heidysnj <heidysnunez15@gmail.com>
2021-02-17 00:14:21 +0000

    fixed up

--- src/main.js
+++ src/main.js
@@ -6,30 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  const button = () => {
-    let value = [
-      "A",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K"
-    ];
-    let symbol = ["&clubs;", "&hearts;", "&diams;", "&spades;"];
-    let randomValuePosition = Math.floor(Math.random() * value.length);
-    let randomSymbolPosition = Math.floor(Math.random() * symbol.length);
-    let randomValue = value[randomValuePosition];
-    let randomSymbol = symbol[randomSymbolPosition];
+  let value = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let symbol = ["&clubs;", "&hearts;", "&diams;", "&spades;"];
+  let randomValuePosition = Math.floor(Math.random() * value.length);
+  let randomSymbolPosition = Math.floor(Math.random() * symbol.length);
+  let randomValue = value[randomValuePosition];
+  let randomSymbol = symbol[randomSymbolPosition];
 
-    document.querySelector(".symbolCardTop").innerHTML = randomSymbol;
-    document.querySelector(".valueCard").innerHTML = randomValue;
-    document.querySelector(".symbolCardBottom").innerHTML = randomSymbol;
-  };
+  document.querySelector(".symbolCardTop").innerHTML = randomSymbol;
+  document.querySelector(".valueCard").innerHTML = randomValue;
+  document.querySelector(".symbolCardBottom").innerHTML = randomSymbol;
 };
